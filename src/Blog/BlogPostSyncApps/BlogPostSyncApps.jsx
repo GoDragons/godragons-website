a7509b5d04693f7f6f3a2d728c96e86682a74814
Dragos <dragos@Ioanas-MacBook-Pro-2.local>
2020-03-19 19:09:35 +0000

    Add CoverImage component for blog posts

--- src/Blog/BlogPostSyncApps/BlogPostSyncApps.jsx
+++ src/Blog/BlogPostSyncApps/BlogPostSyncApps.jsx
@@ -4,20 +4,32 @@ import { withRouter } from "react-router-dom";
 
 import List from "../../Common/List/List";
 
+import FeaturedImagePath from "../../assets/godragons-app-sync.jpg";
+
+import CoverImage from "../../Common/CoverImage/CoverImage";
+import BoxedLayout from "../../Common/BoxedLayout/BoxedLayout";
+
 import "./BlogPostSyncApps.scss";
 
 function BlogPostSyncApps() {
   return (
     <div className="blog-post-sync-apps">
-      <List
-        items={[
-          "Relying on the internal clock of the device, and just periodically reading from it (in theory, this should be enough)",
-          "Pinging a server every second to get the time",
-          "Broadcasting server time every second via websockets",
-          "Combining the first 2: syncing the time every minute, and using the internal clock in-between requests"
-        ]}
-        icon="bullseye"
-      />
+      <BoxedLayout>
+        <CoverImage
+          path={FeaturedImagePath}
+          title="Keeping apps in sync…"
+          subtitle="How hard can it be?"
+        />
+        <List
+          items={[
+            "Relying on the internal clock of the device, and just periodically reading from it (in theory, this should be enough)",
+            "Pinging a server every second to get the time",
+            "Broadcasting server time every second via websockets",
+            "Combining the first 2: syncing the time every minute, and using the internal clock in-between requests"
+          ]}
+          icon="bullseye"
+        />
+      </BoxedLayout>
     </div>
   );
 }
