fee444b9b75ab22177c468b4d155403c351bbb86
dragos <dragos@dragoss-MacBook-Pro.local>
2020-04-03 17:45:18 +0100

    Remove tf directory (it wasn't meant to be here anyway)

--- tf/cloudwatch.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_cloudwatch_log_group" "hiit_tribe" {
-  name = "hiit-tribe"
-}
--- tf/cognito.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_cognito_identity_pool" "hiit_tribe" {
-  identity_pool_name               = "hiit tribe"
-  allow_unauthenticated_identities = true
-}
-
-resource "aws_cognito_user_pool" "hiit_tribe" {
-  name = "hiit tribe"
-}
