0427d86cf49eb317373871a26cdfaf58eaf7d0db
dragosiordachioaia <xdragosy@gmail.com>
2020-04-22 17:34:14 +0100

    Add comment to ScrollToTop

--- src/Common/ScrollToTop/ScrollToTop.jsx
+++ src/Common/ScrollToTop/ScrollToTop.jsx
@@ -4,6 +4,8 @@ import { withRouter } from "react-router-dom";
 class ScrollToTop extends Component {
   componentDidUpdate(prevProps) {
     if (this.props.location !== prevProps.location) {
+      // we have to scroll the #root element instead of the whole window, because its
+      // direct parent (the body element) has overflow: auto and height: 100vh
       document.querySelector("#root").scrollTo(0, 0);
     }
   }
