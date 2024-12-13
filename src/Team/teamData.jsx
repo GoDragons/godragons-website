f34edea40749b295d9a3c71e5e0c48ada4738eee
dragos <xdragosy@gmail.com>
2024-12-13 12:44:05 +0100

    Replace names and photos

--- src/Team/teamData.jsx
+++ src/Team/teamData.jsx
@@ -1,20 +1,18 @@
-import MarcPortrait from "../assets/godragons-marc-portrait.png";
-import DragosPortrait from "../assets/godragons-dragos-portrait.png";
+import ElectraPortrait from "../assets/godragons-electra-portrait.png";
+import DragosPortrait from "../assets/godragons-dragos-portrait.jpg";
 
 const teamData = [
   {
-    name: "Marc Coury",
+    name: "Electra Coury",
     title: "Founder",
-    bio:
-      "Founding partner of GoDragons, Marc Coury, is a highly experienced software developer, public speaker and events organiser. He enjoys finding and delivering solutions to complex problems in a fast paced environment. <br /><br /> At the 2019 Go conference (gophercon.co.uk) in London Marc delivered a workshop on TDD and BDD. Additionally, while studying for his PhD, he led the organisation of a scientific conference (Hermes2014.org) and spoke at many scientific conferences and outreach events. <br /><br /> In his spare time he enjoys cooking, yoga, swimming and meditation.",
-    linkedin: "https://www.linkedin.com/in/marccoury/",
-    photo: MarcPortrait,
+    bio: "Founding partner of GoDragons, Marc Coury, is a highly experienced software developer, public speaker and events organiser. He enjoys finding and delivering solutions to complex problems in a fast paced environment. <br /><br /> At the 2019 Go conference (gophercon.co.uk) in London Marc delivered a workshop on TDD and BDD. Additionally, while studying for his PhD, he led the organisation of a scientific conference (Hermes2014.org) and spoke at many scientific conferences and outreach events. <br /><br /> In his spare time he enjoys cooking, yoga, swimming and meditation.",
+    linkedin: "https://www.linkedin.com/in/electracoury/",
+    photo: ElectraPortrait,
   },
   {
-    name: "Dragos Iordachioaia",
+    name: "Dragos Mitelea",
     title: "Founder",
-    bio:
-      "Dragos Iordachioaia has decided to co-found GoDragons out of a desire to build a truly sustainable business. He has been building commercial software for 10 years, during which he has acquired skills ranging from public speaking and 3D graphics design to marketing and retail. <br /><br /> His strength is distilling complex problems into simple solutions. Dragos also really enjoys sharing knowledge and training people, having done that in most of his roles.<br /><br />His downtime is spent cycling, boxing and playing tennis, mixed with learning new languages.",
+    bio: "Dragos Iordachioaia has decided to co-found GoDragons out of a desire to build a truly sustainable business. He has been building commercial software for 10 years, during which he has acquired skills ranging from public speaking and 3D graphics design to marketing and retail. <br /><br /> His strength is distilling complex problems into simple solutions. Dragos also really enjoys sharing knowledge and training people, having done that in most of his roles.<br /><br />His downtime is spent cycling, boxing and playing tennis, mixed with learning new languages.",
     linkedin: "https://www.linkedin.com/in/dragos-iordachioaia/",
     photo: DragosPortrait,
   },
